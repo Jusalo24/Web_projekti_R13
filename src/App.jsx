2b4790968247bbba1c6262850a46e8cfb05d6ae7
Jusalo24 <jusalo24@students.oamk.fi>
2025-11-07 15:56:07 +0200

    p

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import navbar from './components/navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <nav className= "navbar">
+
+
+
+      </nav>
+
+      <main>
+
+
+      </main>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank"> 
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+export default function navbar () {
+    return (
+        <nav className="navbar">
+            <h1 className="logo1">"Movie Project"</h1>
+            <ul className="Links">
+                <li>Movies</li>
+                <li>Series</li>
+                <li>Reviews</li>
+                <li>Groups</li>
+                <li>Account</li>
+            </ul>
+        </nav>
+    )
+
+}
