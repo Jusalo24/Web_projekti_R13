861c0d2cb7c96e4905453574ac9107c1a0e8147f
LeeviMaatta <lepa2505@gmail.com>
2025-11-11 00:36:01 +0200

    Http hakuja tmdb apista lisätty

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import 'dotenv/config.js'
 import movieRouter from './routes/movieRouter.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/movies', movieRouter)
+app.use('', movieRouter)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}/`))
--- server/routes/movieRouter.js
+++ server/routes/movieRouter.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { } from '../controllers/TMBDMovieController.js'
+import { getMovieById, getMoviesByType } from '../controllers/TMBDMovieController.js'
 
 const movieRouter = Router()
 
 const TMDB_BASE = 'https://api.themoviedb.org/3'
 
+// Hae elokuvia kategorian perusteella (popular, now_playing, top_rated, latest, upcoming) lisää query "?region=FI" niin saat nyt elokuvateattereissa Suomessa
+movieRouter.get('/MoviesByType/:searchType/:page', getMoviesByType)
 
+// Hae elokuvan tiedot ID:n perusteella
+movieRouter.get('/DetailsById/:movieId', getMovieById)
 
 export default movieRouter
