8d543e5e62e4ce686e470252362a950b5ce836b9
Jusalo24 <jusalo24@students.oamk.fi>
2025-11-13 15:43:15 +0200

    ini commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,27 @@
 import { useState, useEffect } from 'react'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Router } from "react-router-dom"
 import './App.css'
 import axios from 'axios'
-import navbar from './components/navbar'
-import Home from './pages/Home'
-import Movies from './pages/Movies'
-import Groups from './pages/Groups'
-
-
-
-
+import Navbar from "./components/navbar"
+import Home from "./pages/Home"
+// Voit lisätä nämä myöhemmin:
+import Movies from "./pages/Movies"
+import Groups from "./pages/Groups"
+import Account from "./pages/Account"
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <Router>
-      <nav className= "Navbar">
-
-
-
-      </nav>
-
-      <main>
-
-
-      </main>
-
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </Router>
   )
 }
 
 export default App
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,11 @@
+import React from 'react'
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom/client"
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
