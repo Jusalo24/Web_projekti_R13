b56f95b448a77b512dd0b363dd53c96cb2cf3296
Jusalo24 <jusalo24@students.oamk.fi>
2025-11-13 16:20:16 +0200

    ini commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from 'react'
-import { Routes, Route, Router } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 import axios from 'axios'
 import Navbar from "./components/navbar"
 import Home from "./pages/Home"
-// Voit lisätä nämä myöhemmin:
 import Movies from "./pages/Movies"
 import Groups from "./pages/Groups"
 import Account from "./pages/Account"
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -19,7 +18,7 @@ function App() {
         <Route path="/groups" element={<Groups />} />
         <Route path="/account" element={<Account />} />
       </Routes>
-    </Router>
+    </>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom/client"
 import { StrictMode } from 'react'
-//import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
